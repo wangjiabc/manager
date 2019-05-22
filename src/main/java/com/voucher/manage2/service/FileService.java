632ddd6a89ac6f6cae739341fcf1e013681ada55
lz-yy <2475214987@qq.com>
2019-05-22 15:33:19 +0800

    5.22.1

--- src/main/java/com/voucher/manage2/service/FileService.java
+++ src/main/java/com/voucher/manage2/service/FileService.java
@@ -1,12 +1,22 @@
 package com.voucher.manage2.service;
 
 
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+/**
+ * @Author lz
+ * @Description: 文件操作
+ * @Date: 2019/5/22 15:32
+ **/
 public interface FileService {
-
-    String fileUpload( MultipartFile file, List<String> roomGuids);
+    /**
+     * @Author lz
+     * @Description: 上传文件
+     * @param: [file, roomGuids]
+     * @return: {java.lang.String}
+     * @Date: 2019/5/22 15:32
+     **/
+    String fileUpload(MultipartFile file, List<String> roomGuids);
 }
