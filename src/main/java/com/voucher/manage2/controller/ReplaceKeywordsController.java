ec144ab95605c128507108a5c92bee8d724a16e2
changshan <401270884@qq.com>
2019-07-01 15:44:05 +0800

    7.2

---
+++ src/main/java/com/voucher/manage2/controller/ReplaceKeywordsController.java
@@ -0,0 +1,32 @@
+package com.voucher.manage2.controller;
+
+import com.voucher.manage2.service.ReplaceKeywordsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author z
+ * @description ´òÓ¡word¿ØÖÆÆ÷
+ * @date 2019/6/26
+ */
+@RestController
+@RequestMapping("/printWord")
+public class ReplaceKeywordsController {
+
+    @Autowired
+    private ReplaceKeywordsService service;
+
+    /**
+     * Ìæ»»¹Ø¼ü×Ö
+     * @throws IOException
+     */
+    @RequestMapping("/acquireData")
+    public void acquireData() throws IOException {
+        service.printWord();
+    }
+
+}
---
+++ src/main/java/com/voucher/manage2/controller/Word2PdfController.java
@@ -0,0 +1,46 @@
+package com.voucher.manage2.controller;
+
+
+import com.voucher.manage2.service.PdfAddressRuturnService;
+import com.voucher.manage2.service.Word2PdfService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+/**
+ * word转PDF文档
+ * 返回PDF文档地址
+ */
+@Controller
+@RequestMapping("/convert")
+public class Word2PdfController {
+
+    @Autowired
+    private PdfAddressRuturnService  ruturnService;
+
+    @Autowired
+    private Word2PdfService word2PdfService;
+
+
+    /**
+     * word转PDF方法
+     */
+    @RequestMapping("/w2p")
+    public void word2Pdf(){
+        String word = "src\\main\\java\\com\\voucher\\docx\\01.docx";
+        String name = "02".concat(".pdf");
+        String pdf = "src\\main\\java\\com\\voucher\\docx\\"+name;
+        word2PdfService.convert(word,pdf);
+    }
+
+    /**
+     * 返回PDF地址
+     * @return
+     */
+    @RequestMapping("/return")
+    public String returnAddress(){
+        return ruturnService.ruturnAddress();
+    }
+}
