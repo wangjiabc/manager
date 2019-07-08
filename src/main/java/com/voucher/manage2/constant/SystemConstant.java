4ebc7f1f5621b44f548651c0ff6dbd8b5b88729b
changshan <401270884@qq.com>
2019-07-08 08:34:15 +0800

    7.3

--- src/main/java/com/voucher/manage2/constant/SystemConstant.java
+++ src/main/java/com/voucher/manage2/constant/SystemConstant.java
@@ -1,6 +1,8 @@
 package com.voucher.manage2.constant;
 
 import com.voucher.manage2.utils.FileUtils;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 
@@ -9,13 +11,22 @@ import java.io.File;
  * @description
  * @date 2019/6/19
  */
-public class SystemConstant {
+@Component
+public class SystemConstant implements InitializingBean {
     public static final String SYSTEM_ROLE_GUID = "07daf7bc5c7d41f7b5b1291d3f275d17";
 
-    public static final String START_WORD_PATH = FileUtils.FILE_PATH;
+    public static final String START_WORD_PATH = FileUtils.FILE_PATH+File.separator+"cache";
     public static final String START_WORD_FILENAME = "00.docx";
     public static final String WORD_SUFFIX = ".docx";
     public static final String PDF_SUFFIX = ".pdf";
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        File file = new File(START_WORD_PATH);
+        if (!file.exists()){
+            file.mkdirs();
+        }
+    }
 
 //    public static final String REPLACE_WORD = "D:" + File.separator + "manage-upload"+"\\01.docx";
 //    public static final String PDF_ADDRESS = "D:" + File.separator + "manage-upload"+"\\02.pdf";
---
+++ src/main/java/com/voucher/manage2/utils/DeleteOnTime.java
@@ -0,0 +1,25 @@
+package com.voucher.manage2.utils;
+
+import com.voucher.manage2.constant.SystemConstant;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+public class DeleteOnTime {
+
+
+    @Scheduled(cron = "0 0 4 * * ?")  //每天4秒执行一次定时任务
+    public void consoleInfo(){
+        System.out.println("定时任务");
+        //File[] files = new File(SystemConstant.START_WORD_PATH).listFiles();
+        File file = new File(SystemConstant.START_WORD_PATH);
+        String[] listFile = file.list();
+        for (String file1 : listFile) {
+            File file2 = new File(file + file1);
+            file2.delete();
+        }
+    }
+
+}
